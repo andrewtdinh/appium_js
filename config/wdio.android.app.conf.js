3611cfc73ec3fc49e7acef1b97e94d149d918164
Andrew Dinh <andrewdinh74@yahoo.com>
2021-05-03 16:33:02 -0700

    Update package.json and config for running android apk

--- config/wdio.android.app.conf.js
+++ config/wdio.android.app.conf.js
@@ -21,14 +21,14 @@ config.capabilities = [
         // For W3C the appium capabilities need to have an extension prefix
         // http://appium.io/docs/en/writing-running-appium/caps/
         // This is `appium:` for all Appium Capabilities which can be found here
-        'appium:deviceName': 'Pixel_8.1',
-        'appium:platformVersion': '8.1',
+        'appium:deviceName': 'Pixel3a_XL_API_30',
+        'appium:platformVersion': '11.0',
         'appium:orientation': 'PORTRAIT',
         // `automationName` will be mandatory, see
         // https://github.com/appium/appium/releases/tag/v1.13.0
         'appium:automationName': 'UiAutomator2',
         // The path to the app
-        'appium:app': join(process.cwd(), './apps/Android-NativeDemoApp-0.2.1.apk'),
+        'appium:app': join(process.cwd(), './Android-NativeDemoApp-0.2.1.apk'),
         // Read the reset strategies very well, they differ per platform, see
         // http://appium.io/docs/en/writing-running-appium/other/reset-strategies/
         'appium:noReset': true,
