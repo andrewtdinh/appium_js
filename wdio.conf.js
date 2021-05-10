9296a38420426e1799a7dcee35bd25b95b27d3fe
Andrew Dinh <andrewdinh74@yahoo.com>
2021-05-10 11:30:55 -0700

    Best version with Appium Server pre-started

--- wdio.conf.js
+++ wdio.conf.js
@@ -10,12 +10,13 @@ exports.config = {
     exclude: [],
     maxInstances: 10,
     capabilities: [{
-        // platformName: 'Android',
-        // 'appium.deviceName': 'Pixel3a_XL_API_30',
-        // 'appium.app': join(process.cwd(), './ApiDemos-debug.apk'),
-        // 'appium.automationName': 'UiAutomator2',
-        // 'appium.platformVersion': '11.0',
-        // 'path': '/wd/hub'
+        platformName: 'Android',
+        deviceName: 'Pixel3a_XL_API_30',
+        app: join(process.cwd(), './ApiDemos-debug.apk'),
+        automationName: 'UiAutomator2',
+        // automationName: 'Appium',  // This also works instead of 'UiAutomator2'
+        platformVersion: '11.0',
+        path: '/wd/hub'
     }],
     logLevel: 'info',
     bail: 0,
@@ -23,19 +24,19 @@ exports.config = {
     waitforTimeout: 10000,
     connectionRetryTimeout: 120000,
     connectionRetryCount: 3,
-    // services: ['selenium-standalone'],
-    services: [
-      ['appium', { 
-        command: 'appium',
-        args: {
-          platformName: 'Android',
-          deviceName: 'Pixel3a_XL_API_30',
-          app: join(process.cwd(), './ApiDemos-debug.apk'),
-          automationName: 'Appium',
-          platformVersion: '11.0'
-        } 
-      }]
-    ],
+    services: ['selenium-standalone'],
+    // services: [
+    //   ['appium', { 
+    //     command: 'appium',
+    //     args: {
+    //       platformName: 'Android',
+    //       deviceName: 'Pixel3a_XL_API_30',
+    //       app: join(process.cwd(), './ApiDemos-debug.apk'),
+    //       automationName: 'Appium',
+    //       platformVersion: '11.0'
+    //     } 
+    //   }]
+    // ],
     framework: 'mocha',
     reporters: ['spec'],
     mochaOpts: {
